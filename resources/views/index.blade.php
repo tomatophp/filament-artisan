47f62405d961278def6e2fdde605bf4e843f4f25
Fady Mondy <info@3x1.io>
2024-06-24 18:12:12 +0300

    remove custom style

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,10 +1,9 @@
-<x-filament::page>
-    @include('filament-artisan::partials.styles')
+<x-filament-panels::page>
     <div id="app">
         <app home="{{ url(config('filament-artisan.home', '/')) }}" endpoint={{ url('admin/artisan/json') }} />
     </div>
-</x-filament::page>
+</x-filament-panels::page>
 
 @push('scripts')
-@include('filament-artisan::partials.scripts')
+    @include('filament-artisan::partials.scripts')
 @endpush
